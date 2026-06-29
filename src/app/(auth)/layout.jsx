971e39956f597b1f7b588a89c49550f4070a1e72
Modina Khusbu Priya <modinakhusbupriya@gmail.com>
2026-06-29 14:07:03 +0300

    Add basic setup

---
+++ src/app/(auth)/layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/shared/Navbar';
+import React from 'react';
+
+const layout = ({ children }) => {
+    return (
+        <div>
+           <Navbar></Navbar> 
+        </div>
+    );
+};
+
+export default layout;
