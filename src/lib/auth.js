25e3691ddd37b635e2b808677365cca12fc4dabd
Modina Khusbu Priya <modinakhusbupriya@gmail.com>
2026-07-07 16:49:52 +0300

    update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -30,13 +30,11 @@ export const auth = betterAuth({
         google: { 
             clientId: process.env.GOOGLE_CLIENT_ID, 
             clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-        }, 
-  },
-
-  socialProviders: {
+        },
         github: { 
             clientId: process.env.GITHUB_CLIENT_ID, 
             clientSecret: process.env.GITHUB_CLIENT_SECRET, 
         }, 
-    },
+  },
+
 });
