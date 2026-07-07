17dc997662981d70ca57dfa81836e9ffd04e7700
Modina Khusbu Priya <modinakhusbupriya@gmail.com>
2026-07-07 21:26:35 +0300

    Add animated loading

--- src/app/(main)/category/[categoryId]/loading.jsx
+++ src/app/(main)/category/[categoryId]/loading.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
 
 const loading = () => {
     return (
         <div className="flex justify-center items-center h-[85vh]">
-            <span className="loading loading-spinner loading-xl"></span>
+            <DotLottieReact
+                src="https://lottie.host/08adcc6e-809c-4504-9966-ee350f5958cc/Gwn2lvKxvP.lottie"
+                loop
+                autoplay
+                />
+
         </div>
     );
 };
